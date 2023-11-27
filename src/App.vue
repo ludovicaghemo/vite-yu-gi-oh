7c8ea5dafe5e2b76b03d32bc160c3604396cf845
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-11-27 15:08:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+//importare AppHeader 
+import AppHeader from './components/AppHeader.vue';
+
+//dichiarare AppHeader 
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+<!-- utilizzare AppHeader -->
+<AppHeader />
+
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<template>
+    
+</template>
+
+<style lang="scss" scoped>
+@use "../style/partials/mixin" as *;
+@use "../style/partials/variables" as *;
+
+</style>
