2e1022ac1f291091faea248266e7397bbf0dde79
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-11-28 15:30:28 +0100

    add: select component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script>
-//importare AppHeader 
+//importare componenti
 import axios from "axios";
 import AppHeader from './components/AppHeader.vue';
 import { store } from "./store.js";
 import AppBody from './components/AppBody.vue';
+import AppSelect from "./components/AppSelect.vue";
 
-//dichiarare AppHeader 
+//dichiarare componenti
 export default {
   data() {
     return {
@@ -15,7 +16,12 @@ export default {
   created() {
     this.store.loading = true
     axios
-      .get(this.store.apiUrl)
+      .get(this.store.apiUrl, {
+        params: {
+          num: 20,
+          offset: 0
+        },
+      })
       .then((resp) => {
         //console.log(resp);
         this.store.cardsList = resp.data;
@@ -25,13 +31,28 @@ export default {
   components: {
     AppHeader,
     AppBody,
-},
+    AppSelect
+  },
+  methods: {
+    handleSelect() {
+      axios.get(this.store.apiArchUrl, {
+        params: {
+          archetype: this.store.selectedOption,
+          num: 20,
+          offset: 0
+        }
+      }).then((resp) => {
+        this.store.cardsList = resp.data;
+      })
+    }
+  },
 };
 </script>
 
 <template>
-  <!-- utilizzare AppHeader e AppBody-->
+  <!-- utilizzare componenti-->
   <AppHeader />
+  <AppSelect @showCards="handleSelect" />
   <AppBody />
 </template>
 
--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
-import { reactive } from "vue";
+import { reactive, } from "vue";
 
 export const store = reactive({
   cardsList: [],
   apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+  apiArchUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype",
   loading: false,
+  selectedOption: "",
 });
