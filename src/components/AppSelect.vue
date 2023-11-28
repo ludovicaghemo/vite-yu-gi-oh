13cda586f9d3ac63459ec87ca2c3052bf0e0bf1e
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-11-28 15:42:16 +0100

    fix: select

--- src/components/AppSelect.vue
+++ src/components/AppSelect.vue
@@ -12,6 +12,7 @@ export default {
 <template>
     <select class="form-select w-25" aria-label="Default select example" v-model="store.selectedOption"
         @change="$emit('showCards')">
+        <option value="">Choose an archetype</option>
         <option value="Alien">Alien</option>
         <option value="Ally of Justice">Ally of Justice</option>
         <option value="Ancient Gear">Ancient Gear</option>
